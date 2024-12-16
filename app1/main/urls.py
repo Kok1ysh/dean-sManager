0b000c4e5a2d1a7ee9f0f2020c41b95444c9f9f1
Kok1ysh <yalovenko.lybomur@gmail.com>
2024-12-16 23:12:37 +0200

    AddViewRozklad

--- app1/main/urls.py
+++ app1/main/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index,name='home'),
+    path('groups/', views.groups, name='groups'),
+    path('predmet/', views.predmet, name='predmet'),
     path('profil/', views.profil,name='profil'),
 ]
--- app1/main/views.py
+++ app1/main/views.py
@@ -1,12 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from control.models import Facultys
-
+from group.models import Groups
+from rozklad.models import Rozklad
 
 
 def index(request):
-    facultys=Facultys.objects.all()   
-    return render(request,'main/home.html',{'facultys':facultys})
+    facultys=Facultys.objects.all() 
+    context = {'facultys':facultys}
+    return render(request,'main/home.html', context)
+
+def groups(request):
+    faculty = request.GET.get('faculty')
+    groups = Groups.objects.filter(faculty=faculty)
+    context = {'groups': groups}
+    return render(request, 'main/groups.html', context)
+
+def predmet(request):
+    grupa = request.GET.get('group')
+    predmet = Rozklad.objects.filter(grupa=grupa).first()
+    print(predmet)
+    context = {'predmet': predmet}
+
+    return render(request, 'main/predmet.html', context)
 
 def profil(request):
        
